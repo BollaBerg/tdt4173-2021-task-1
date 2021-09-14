bc38a3bfeeb22e68239538a0f341f2287da3761a
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2021-09-14 13:31:13 +0200

    Add first test of logistic regression

---
+++ logistic_regression/test_logistic_regression.py
@@ -0,0 +1,17 @@
+import numpy as np
+from pytest import approx
+
+from logistic_regression import LogisticRegression, sigmoid
+
+def test_predict_multiplies_weights_correctly():
+    """Test that LogisticRegression.predict() correctly multiplies weights"""
+    reg = LogisticRegression()
+    reg.weights = np.array([-1, 2])
+    predictions = reg.predict(np.array([[1, 2], [3, 4], [11, 22]]))
+
+    actual_predictions = sigmoid(np.array([[-1, 4], [-3, 8], [-11, 44]]))
+
+    np.testing.assert_array_equal(
+        predictions, actual_predictions,
+        "Weights were not multiplied correctly"
+    )
