ada5891f38eb0e93b63b3a3b7ede5b9e7b9186c6
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2021-09-15 14:20:24 +0200

    Fix error in tests of logistic regression

--- logistic_regression/test_logistic_regression.py
+++ logistic_regression/test_logistic_regression.py
@@ -5,7 +5,7 @@ from logistic_regression import LogisticRegression, sigmoid
 
 def test_predict_multiplies_weights_correctly():
     """Test that LogisticRegression.predict() correctly multiplies weights"""
-    reg = LogisticRegression()
+    reg = LogisticRegression(has_intercept=False)
     reg.weights = np.array([-1, 2])
     predictions = reg.predict(np.array([[1, 2], [3, 4], [11, 22]]))
 
